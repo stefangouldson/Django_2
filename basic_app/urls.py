be29ee671f5826eb4aa4b6d9fc5554c7832a81ff
Stefan Gouldson <stefangouldson@gmail.com>
2019-11-26 16:14:15 +0000

    first commit

---
+++ basic_app/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from basic_app import views
+from django.urls import path
+
+# SET THE NAMESPACE!
+app_name = 'basic_app'
+
+# Be careful setting the name to just /login use userlogin instead!
+urlpatterns=[
+    path('register/$',views.register,name='register'),
+    path('user_login/$',views.user_login,name='user_login'),
+]
